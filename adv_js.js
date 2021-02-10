743c3acd163b509e137f8bd2fa0a78c2d2fd707c
jannath <jannathsyeda7@gmail.com>
2021-02-10 22:49:13 +0600

    all adv functions end

--- adv_js.js
+++ adv_js.js
@@ -278,3 +278,22 @@ console.log(resultSplice);
 const arr=[5,8,6,4];
 const resultjoin=arr.join("*");
 console.log(resultjoin);
+
+
+//break continue
+  
+const nums = [1,-2,3,4,-5,-6,7,-8,9];
+for(let i = 0; i < nums.length; i++){
+    
+    if(nums[i]>3){
+        break;
+    }
+    //console.log(nums[i]);
+}
+
+for(let i = 0; i < nums.length; i++){
+    if(nums[i]<0){
+        continue;
+    }
+    console.log(nums[i]);
+}
