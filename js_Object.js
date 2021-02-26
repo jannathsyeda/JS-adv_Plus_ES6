7a9ffc98ae4ece070e82c09637e4882fa2ad2285
unknown <jannathsyeda7@gmail.com>
2021-02-26 19:32:23 +0600

    end

--- js_Object.js
+++ js_Object.js
@@ -1,4 +1,4 @@
-//bind ,call  and apply 
+console.log("------------bind ,call  and apply -----------------")
 const Object1={
     name:"fahima",
     id:33,
@@ -41,3 +41,26 @@ console.log('call result=',object_bind.salary);
 Object1.chargeBill.call(object_bind,[20000]);//here object_bind is this value of chargebill
 console.log('apply result=',object_bind.salary);
 
+
+console.log("-------------window, global variable, global scope------------------------------------")
+
+//window   javascript er exicution
+//defination-javascript variable  window is an object,representing a window containing dom document
+document === window.document
+console === window.console
+//implicitly global hoy tokon ,jokon function er betor variable var na likhey declare kora hoy
+//function er betor variablle local scope e thakey,oi function er betorer variable ke function er bahir thekey use kora jay na
+console.log("---new keyword, class and object difference--------------------------")
+//cls obj different
+// cls e property ar method takey,and 1 ti cls theke onek gulo object create kora jay,new key 
+//question :prototypical inheritance ki?
+
+console.log("---------thethiskeyword-----");//vvimp
+
+    // this key word->The keyword  you use to refer to an object through which they were invoked is
+
+
+    //Javascript works event loop stack and queue
+// js works 1st as astack,then works with queue
+//synchronus kaj shes kore waiting list er kaj i mean asynchronus kaj kore
+//new date();for current date
