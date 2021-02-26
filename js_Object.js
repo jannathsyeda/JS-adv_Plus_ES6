d498cfc4b5235fc0e6b8091fab7977034e0f8b02
unknown <jannathsyeda7@gmail.com>
2021-02-26 13:47:44 +0600

    call,apply,bind

--- js_Object.js
+++ js_Object.js
@@ -1,4 +1,4 @@
-//call apply 
+//bind ,call  and apply 
 const Object1={
     name:"fahima",
     id:33,
@@ -15,11 +15,14 @@ return this.salary;
 
  Object1.getFunctionMethod();
  Object1.chargeBill(1000);
- console.log('salary is=',Object1.salary);
+ console.log('normal object 1st salary is=',Object1.salary);
 
- //bind to borrow method from another object
+ //bind 
+ //bind->to borrow method from another object
 //q:ekta object er method ke onno object e add korar 3ta system ase
 //1->bind(less efficient)return hishebe onno arekti function dibe
+//2->call
+//apply
 const object_bind={
     Firstname:"jash",
     LastName:"mitu",
@@ -28,4 +31,13 @@ const object_bind={
 
 const charge_bil_in_bind=Object1.chargeBill.bind(object_bind);
 charge_bil_in_bind(20000);
-console.log(object_bind.salary);
+console.log('bind result=',object_bind.salary);
+
+//call (this value and argument parameter ke coma dia seperate kore)onno object er function ke nije kore na eney direct call kore
+Object1.chargeBill.call(object_bind,20000);//here object_bind is this value of chargebill
+console.log('call result=',object_bind.salary);
+
+//apply(array hishabe argument patano lage,1st this argument sara)
+Object1.chargeBill.call(object_bind,[20000]);//here object_bind is this value of chargebill
+console.log('apply result=',object_bind.salary);
+
